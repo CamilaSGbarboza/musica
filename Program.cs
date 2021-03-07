200e50b43b58f37add6218b6bba55923f9bb6aec
camila <camilasangobar@gmail.com>
2021-03-06 23:01:46 -0300

    snowman

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine("SNOWMAN");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sia");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Don't cry, snowman, not in front of me");
+            Console.WriteLine("Who will catch your tears if you can't catch me, darling?");
+            Console.WriteLine("If you can't catch me, darling");
+            Console.WriteLine("Don't cry, snowman, don't leave me this way");
+            Console.WriteLine("A puddle of water can't hold me close, baby");
+            Console.WriteLine("Can't hold me close, baby");
+            Console.WriteLine();
+            Console.WriteLine("I want you to know that I'm never leaving");
+            Console.WriteLine("Cause I'm Mrs. Snow till death we'll be freezing");
+            Console.WriteLine("Yeah, you are my home, my home for all seasons");
+            Console.WriteLine("So come on, let's go");
+            Console.WriteLine();
+            Console.WriteLine("Let's go below zero and hide from the Sun");
+            Console.WriteLine("I love you forever where we'll have some fun");
+            Console.WriteLine("Yes, let's hit the North Pole and live happily");
+            Console.WriteLine("Please, don't cry, no tears now");
+            Console.WriteLine("It's Christmas, baby");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("My snowman and me");
+            Console.WriteLine("My snowman and me");
+            Console.WriteLine("Baby");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Don't cry, snowman, don't you fear the Sun");
+            Console.WriteLine("Who'll carry me without legs to run, honey?");
+            Console.WriteLine("Without legs to run, honey");
+            Console.WriteLine("Don't cry, snowman, don't you shed a tear");
+            Console.WriteLine("Who'll hear my secrets if you don't have ears,");
+            Console.WriteLine("baby");
+            Console.WriteLine();
+            Console.WriteLine("If you don't have ears, baby");
+            Console.WriteLine("I want you to know that I'm never leaving");
+            Console.WriteLine("Cause I'm Mrs. Snow till death will be freezing");
+            Console.WriteLine("You are my home, my home for all seasons");
+            Console.WriteLine("So come on, let's go");
+            Console.WriteLine();
+            Console.WriteLine("Let's go below zero and hide from the Sun");
+            Console.WriteLine("I love you forever where we'll have some fun");
+            Console.WriteLine("Yes, let's hit the North Pole and live happily");
+            Console.WriteLine("Please, don't cry, no tears now");
+            Console.WriteLine("It's Christmas, baby");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("My snowman and me");
+            Console.WriteLine("My snowman and me");
+            Console.WriteLine("Baby");
+            Console.WriteLine();
+            Console.ResetColor();
+
+
+            
+
+            
+
+
+        }
+    }
+}
